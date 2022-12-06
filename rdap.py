d469fd0a8cd4fae30bac3a8181bb647645b45b30
John Spounias <d3cline@pyl0n.com>
2022-12-05 21:05:45 -0700

    some tlds aren't on the list

--- rdap.py
+++ rdap.py
@@ -13,18 +13,16 @@ TLD_API_MAP = get_tld_map()
 def find_api_endpoint(tld):
   for x in TLD_API_MAP:
     if tld in x[0]: return x[1][0]
+    elif tld == 'de': return 'https://rdap.denic.de/'
 
 def rdap(url):
   pass
 
-
-
-
-
 grabbed = grab_outside()
 for instance in grabbed:
   domain = instance['domain']
-  res = get_tld(domain, as_object=True, fix_protocol=True)
+  try: res = get_tld(domain, as_object=True, fix_protocol=True)
+  except: res = None
   if res:
     res.subdomain
     # 'some.subdomain'
@@ -36,7 +34,7 @@ for instance in grabbed:
     # 'google.co.uk'
 
     api_endpoint = find_api_endpoint(res.tld.encode('idna').decode())
-    print(f'{api_endpoint}domain/{domain}')
+    print(f'{api_endpoint}domain/{res.fld}')
 
     """
     rdap_info = rdap(res.fld)
