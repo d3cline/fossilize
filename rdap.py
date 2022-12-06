44f1b85613b3a3405a72a42406178f0f377cab2b
John Spounias <d3cline@pyl0n.com>
2022-12-05 20:48:45 -0700

    raw rdap, no go

--- rdap.py
+++ rdap.py
@@ -1,10 +1,50 @@
-import subprocess
-from pathlib import Path
-home_dir = Path.home()
+import requests
 import json 
+from tld import get_tld
+from grab import grab_outside
+
+def get_tld_map():
+  rdap_api_endpoints = requests.get(url='https://data.iana.org/rdap/dns.json').json()['services']
+  print("TLD API MAP OBTAINED")
+  return rdap_api_endpoints
+
+TLD_API_MAP = get_tld_map()
+
+def find_api_endpoint(tld):
+  for x in TLD_API_MAP:
+    if tld in x[0]: return x[1][0]
 
 def rdap(url):
-  try:
-    return json.loads(subprocess.run([f"{home_dir}/go/bin/rdap", "--json", url], capture_output=True).stdout.strip())
-  except:
-    return None
+  pass
+
+
+
+
+
+grabbed = grab_outside()
+for instance in grabbed:
+  domain = instance['domain']
+  res = get_tld(domain, as_object=True, fix_protocol=True)
+  if res:
+    res.subdomain
+    # 'some.subdomain'
+    res.domain
+    # 'google'
+    res.tld
+    # 'co.uk'
+    res.fld
+    # 'google.co.uk'
+
+    api_endpoint = find_api_endpoint(res.tld.encode('idna').decode())
+    print(f'{api_endpoint}domain/{domain}')
+
+    """
+    rdap_info = rdap(res.fld)
+    f = open(f'rdaps/{res.fld}.json', "w")
+    f.write(json.dumps(rdap_info))
+    f.close()
+    print(f"ADDED {instance['domain']}")
+    """
+
+  else:
+    print(f"FAILED {instance['domain']}")
